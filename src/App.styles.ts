8eb01e6cf8b2efe7c20943cfdc040d13fff88e3c
Gottfried Weimer <gottfried.weimer@blockframe.de>
2022-03-07 12:12:11 +0100

    bugfix now fully responsive again

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,7 +1,7 @@
 import { Paper, styled } from '@mui/material'
 
 export const StyledPaper = styled(Paper)(() => ({
-	width: '100vh',
+	width: '100vw',
 	maxWidth: 375,
 	height: '100vh',
 	maxHeight: 667,
