a317eee22b8a6df9c69a7793b7ead9cea0e0f500
Gottfried Weimer <gottfried.weimer@blockframe.de>
2022-03-09 22:38:50 +0100

    removed app dimension boundaries

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,7 +2,7 @@ import { Paper, styled } from '@mui/material'
 
 export const StyledPaper = styled(Paper)(() => ({
 	width: '100vw',
-	maxWidth: 375,
+	// maxWidth: 375,
 	height: '100vh',
-	maxHeight: 667,
+	// maxHeight: 667,
 }))
